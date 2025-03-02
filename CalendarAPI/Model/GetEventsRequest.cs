2ceecbf1eb6f6381e72a6760b526934a2cb45c2e
Andrew Baxter <rhinocratic@googlemail.com>
2025-03-02 19:54:34 +0000

    SAVEPOINT

--- CalendarAPI/Model/GetEventsRequest.cs
+++ CalendarAPI/Model/GetEventsRequest.cs
@@ -1,8 +1,20 @@
+using FastEndpoints;
+using FluentValidation;
+
 namespace CalendarAPI.Model;
 
 public class GetEventsRequest
 {
-    public int Year {get; set;}
-    public int Month {get; set;}
-    public int? Day {get; set;}
+    public int Year { get; set; }
+    public int Month { get; set; }
+    public int? Day { get; set; }
+    // public Tuple<int> YMD { get; set; }
+
+    internal sealed class Validator : Validator<GetEventsRequest>
+    {
+        public Validator()
+        {
+
+        }
+    }
 }
